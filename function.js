4f33aa0203110143ae25d8124e679d432ff9c4e9
Chubbi-Stephen <udeokoliestephen@gmail.com>
2022-07-26 14:59:22 +0100

    Trying to fix a bug

---
+++ function.js
@@ -0,0 +1,22 @@
+// let ratingUnit = document.querySelector(".rating__unit");
+
+// ratingUnit.addEventListener("click", ratingGesture);
+
+
+
+// function ratingGesture() {
+//     document.querySelector(".rating__unit").style.backgroundColor = hsl(25, 97%, 53%);
+
+//     console.log("Happy man")
+    
+// }
+
+const rating = document.querySelectorAll("#unit1, #unit2, #unit3");
+
+rating.forEach(rate => {
+    addEventListener("click", console.log("hello")) 
+})
+
+// function rate() {
+//     console.log("Hello")
+// }
